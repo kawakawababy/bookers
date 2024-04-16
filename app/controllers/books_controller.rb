d91d4357b77b0d852da8d263f469e087a4bbe24c
kawakawababy <163483969+kawakawababy@users.noreply.github.com>
2024-04-16 23:28:16 +0000

    [Finish] フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,16 @@
 class BooksController < ApplicationController
-  
-    
-  
+
+
+
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      render :book_path
+    end
   end
 
   def index
@@ -24,14 +28,19 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      render :books/:id
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
   end
 
   private
